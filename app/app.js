2e0809df168bcffc32bfb017b8eff9610e27db08
porma <pormahd@gmail.com>
2022-02-08 16:08:49 -0500

    Add 'new task' frontend

--- app/app.js
+++ app/app.js
@@ -1,7 +1,39 @@
 const app = Vue.createApp({
   data() {
     return {
-      hello: "Hello world"
+      tasks: [],
+      inputTaskName: '',
+      inputTaskDesc: '',
+      // taskValid: false
+    }
+  },
+  computed: {
+    taskValid() {
+      return this.inputTaskName !== '';
+    }
+  },
+  methods: {
+    addTask() {
+
+      // budget validation
+      if (this.inputTaskName === '') return
+
+      // create task object
+      const task = {
+        id: this.tasks.length,
+        name: this.inputTaskName,
+        desc: this.inputTaskDesc
+      }
+
+      // allow modal to close and add task to array
+      this.taskValid = true;
+      this.tasks.push(task);
+
+      // todo: POST request
+
+      // reset input fields
+      this.inputTaskName = '';
+      this.inputTaskDesc = '';
     }
   }
 });
